1d11669219d66e42cdd4c1d5634e3ee5e82ea693
Oliver Dunn <oliverdunndev@gmail.com>
2025-07-10 11:41:24 +0100

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ export default function Contact() {
     <section id="contact">
       <h2>Contact</h2>
       <p className="project-card">📧 <a href="mailto:oliverdunndev@gmail.com">oliverdunndev@gmail.com</a></p>
-      <p className="project-card">🌐 <a href="https://github.com/OliverDunnDev" target="_blank">plennock.github.io</a></p>
+      <p className="project-card">🌐 <a href="https://github.com/OliverDunnDev" target="_blank">Github</a></p>
       <p className="project-card">🔗 <a href="https://linkedin.com/in/oliver-dunn-a404a4251/" target="_blank">LinkedIn</a></p>
     </section>
   );
